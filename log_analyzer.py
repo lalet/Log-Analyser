a9e327831ea857566c8275249ed637bf39f84656
Lalet Scaria <laletscaria@yahoo.co.in>
2017-03-29 15:25:51 -0400

    Updated code for 2nd question

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,7 +1,11 @@
 from pyspark import SparkConf
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
+from operator import add
 import argparse
+
+
+
 
 conf = SparkConf().setAppName("Log Analyser")
 sc = SparkContext(conf=conf)
@@ -10,3 +14,15 @@ sqlCtx = SQLContext(sc)
 textFile = sc.textFile("iliad")
 
 print textFile.count()
+
+def filter_user_achilles(line):
+  if "Starting Session" in line and "user achille" in line:
+    return line
+
+
+counts = textFile.flatMap(lambda x:[x]) \
+		 .filter(lambda x:filter_user_achilles(x)) \
+                 .map(lambda x: (1, 1)) \
+                 .reduceByKey(lambda x, y: x + y).collect()
+
+print counts
