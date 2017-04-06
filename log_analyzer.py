c0752ab2b36daa650e20ff2dd437bd680d798a13
lalet <laletscaria@yahoo.co.in>
2017-04-05 22:19:25 -0400

    Assignment anonymisatino logic completed

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,7 +2,6 @@ from pyspark import SparkConf
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.types import Row
-pyspark.sql.DataFrame 
 from operator import add
 import argparse
 
@@ -15,7 +14,7 @@ sqlCtx = SQLContext(sc)
 
 textFile = sc.textFile("iliad")
 
-textFileCombined = sc.textFile("iliad,odyssey")
+textFileTwo= sc.textFile("odyssey")
 
 print textFile.count()
 
@@ -31,59 +30,95 @@ def filter_find_user(line):
 
 def get_system_name(line):
   words = line.strip().split(" ")
-  for word in words:
-    if "iliad"==word:
-      return 1
-    elif "odyssey"==word:
-      return 2
-  return 0
+  print words
+  return words[3]
 
-counts = textFile.flatMap(lambda x:[x]) \
-		 .filter(lambda x:filter_user_achilles(x)) \
-                 .map(lambda x: (1, 1)) \
-                 .reduceByKey(lambda x, y: x + y).collect()
+#counts = textFile.flatMap(lambda x:[x]) \
+		 #.filter(lambda x:filter_user_achilles(x)) \
+                 #.map(lambda x: (1, 1)) \
+                 #.reduceByKey(lambda x, y: x + y).collect()
 
-print counts
+#print counts
+
+#users = textFile.flatMap(lambda x:[x]) \
+#                .filter(lambda line:"systemd: Starting Session " in line ) \
+#                .map(lambda line: str(line.split("user")[-1].strip()[:-1])) \
+#     		.distinct()  \
+#		.collect()
+
+#session = textFile.flatMap(lambda x:[x]) \
+#                  .filter(lambda line:"systemd: Starting Session " in line ) \
+#                  .map(lambda line: (str(line.split("user")[-1].strip()[:-1]),1)) \
+#		  .reduceByKey(lambda x, y: x + y) \
+#                  .collect()
+
+#errors = textFile.flatMap(lambda x:[x]) \
+#                 .filter(lambda line:"error" in (line.lower()) ) \
+#                 .map(lambda line: (1,1)) \
+#                 .reduceByKey(lambda x, y: x + y) \
+#                 .collect()
+
+#error_counts = textFile.flatMap(lambda x:[x]) \
+#               	       .filter(lambda line:"error" in (line.lower())) \
+#                       .map(lambda line: (line.split("iliad")[1],1)) \
+#                       .reduceByKey(lambda x, y: x + y) \
+#	               .sortBy(lambda x: x[1],ascending=False) \
+#                       .collect()
+
+unique_users = textFileTwo.flatMap(lambda x:[x]) \
+               		       .filter(lambda line:"systemd: Starting Session " in line) \
+               		       .map(lambda line:(str(line.split("user")[-1].strip()[:-1]),"odyssey")) \
+			       .filter(lambda line:"None" not in line) \
+               		       .distinct() 
+			      # .collect()
+
+unique_users_iliad = textFile.flatMap(lambda x:[x]) \
+                               .filter(lambda line:"systemd: Starting Session " in line) \
+                               .map(lambda line:(str(line.split("user")[-1].strip()[:-1]),"iliad")) \
+                               .filter(lambda line:"None" not in line) \
+                               .distinct() 
+                              # .collect()
+
+combined_rdd = unique_users.union(unique_users_iliad)
+
+multiple_hosts = combined_rdd.reduceByKey(lambda x,y:x+","+y) \
+			   .map(lambda x:x[0] if len(x[1].split(",")) > 1 else None)  \
+			   .collect()
+
+multiple_hosts=[x for x in multiple_hosts if None!=x]
+print multiple_hosts
+
+
+single_login = combined_rdd.reduceByKey(lambda x,y:x+","+y) \
+                           .map(lambda x:x if len(x[1].split(","))==1 else None)  \
+                           .collect()
+
+single_login=[x for x in single_login if None!=x]
+print single_login
 
 users = textFile.flatMap(lambda x:[x]) \
                 .filter(lambda line:"systemd: Starting Session " in line ) \
                 .map(lambda line: str(line.split("user")[-1].strip()[:-1])) \
-     		.distinct()  \
-		.collect()
+                .distinct()  \
+		.sortBy(lambda x:x) \
+                .collect()
 
-session = textFile.flatMap(lambda x:[x]) \
-                  .filter(lambda line:"systemd: Starting Session " in line ) \
-                  .map(lambda line: (str(line.split("user")[-1].strip()[:-1]),1)) \
-		  .reduceByKey(lambda x, y: x + y) \
-                  .collect()
-
-errors = textFile.flatMap(lambda x:[x]) \
-                 .filter(lambda line:"error" in (line.lower()) ) \
-                 .map(lambda line: (1,1)) \
-                 .reduceByKey(lambda x, y: x + y) \
-                 .collect()
-
-error_counts = textFile.flatMap(lambda x:[x]) \
-               	       .filter(lambda line:"error" in (line.lower())) \
-                       .map(lambda line: (line.split("iliad")[1],1)) \
-                       .reduceByKey(lambda x, y: x + y) \
-	               .sortBy(lambda x: x[1],ascending=False) \
-                       .collect()
-
-#unique_users = textFileCombined.flatMap(lambda x:[x]) \
-#               		       .filter(lambda line:"systemd: Starting Session " in line) \
-#               		       .map(lambda line:(str(line.split("user")[-1].strip()[:-1]),get_system_name(line))) \
-#				       .filter(lambda line:"None" not in line) \
-#			       .reduceByKey(lambda a:a+a)  \
-#               		       .distinct() \
-#			       .collect()
-
-df = textFile.map(lambda r: Row(r)).toDF(["line"])
-df_errors = df.filter(col("line").like("%ERROR%"))
-df_errors.show()
 
 print users
-print session
-print errors
-print error_counts[0:5]
-print unique_users
+
+anonymised_file = textFile.flatMap(lambda x:[x]) \
+                          .filter(lambda line:"systemd: Starting Session " in line ) \
+                          .map(lambda line: line.replace(str(line.split("user")[-1].strip()[:-1]),"user-"+str(users.index(str(line.split("user")[-1].strip()[:-1]))))) \
+			  .distinct() \
+			  .sortBy(lambda x: x[0]) \
+			  .collect()
+
+print anonymised_file
+			  
+
+#print session
+#print errors
+#print error_counts[0:5]
+#print unique_users
+#print unique_users_iliad
+#print unique_users.union(unique_users_iliad)
